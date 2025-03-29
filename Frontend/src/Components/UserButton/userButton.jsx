fe1a3a934b38a37bbc9ae9407944c9365c49c95a
Koooowal <jan.kowalski4b@o2.pl>
2025-03-29 17:19:09 +0100

    Update userButton.jsx

--- Frontend/src/Components/UserButton/userButton.jsx
+++ Frontend/src/Components/UserButton/userButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './userButton.css'
 import { useState } from 'react';
-import Image from '../Image/Image'
+import Image from '../Image/image'
 import apiRequest from '../../Utility/apiRequest';
 import { useNavigate } from 'react-router';
 import useAuthStore from '../../Utility/authStore';
